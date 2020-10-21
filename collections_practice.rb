e31a18c56e19f66e5bc74d4c4ddff42c09fcd685
jomapormentilla <jomapormentilla@gmail.com>
2020-10-21 19:01:16 -0400

    commit tree 3d52cd9a7fa8a6cde381c4d18328b03b2bb037c1

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,69 @@
+def sort_array_asc( array )
+    array.sort do |a,b|
+        a <=> b
+    end
+end
 
+def sort_array_desc( array )
+    arr = array.sort do |a,b|
+        a <=> b
+    end
+
+    arr.reverse
+end
+
+def sort_array_char_count( array )
+    array.sort do |a,b|
+        a.length <=> b.length
+    end
+end
+
+def swap_elements( array )
+    string = array[1]
+    array[1] = array[2]
+    array[2] = string
+
+    array
+end
+
+def reverse_array( array )
+    array.reverse
+end
+
+def kesha_maker( array )
+    count = 1
+    arr = []
+    array.each do |str|
+        str[2] = "$"
+        arr << str
+    end
+
+    arr
+end
+
+def find_a( array )
+    array.select do |str|
+        str.start_with?("a")
+    end
+end
+
+def sum_array( array )
+    array.inject do |sum, n|
+        sum + n
+    end
+end
+
+def add_s( array )
+    count = 0
+    arr = []
+    while count < array.length
+        if count == 1
+            arr << array[count]
+        else
+            arr << "#{ array[count] }s"
+        end
+        count += 1
+    end
+    
+    arr
+end
